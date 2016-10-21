cfca6fc677feca4adfd8552426fe8592cadcdf3f
chimp <chimp@LAPTOP-NN62FHI2>
2016-10-21 18:27:08 -0400

    View Update

--- src/Record.java
+++ src/Record.java
@@ -54,13 +54,14 @@ public class Record {
 	}
 	
 	public void showRecords() {
-		System.out.println("Obs Name, Measure, Obs Date, Obs Value, Record Date");
+		//System.out.println("Obs Name, Measure, Obs Date, Obs Value, Record Date");
 		for (int i = 0; i < obsName.length; i++) {
-			System.out.print(obsName[i]+",");
-			System.out.print(measure[i]+",");
-			System.out.print(obsDate[i]+",");
-			System.out.print(obsValue[i]+",");
-			System.out.println(recordDate[i]);
+			System.out.print("Obs Name: "+obsName[i]+",");
+			System.out.print("Measure: "+measure[i]+",");
+			System.out.print("Obs Date: "+obsDate[i]+",");
+			System.out.print("Obs Value: "+obsValue[i]+",");
+			System.out.println("Record Date: "+recordDate[i]);
+			System.out.println("\n");
 		}
 	}
 }
